e21121052016c4b7cd72b7b5d3e497658c203ab6
LuaSecurity <211252621+LuaSecurity@users.noreply.github.com>
2025-11-01 14:24:38 -0300

    Update Protection.lua

--- Protection.lua
+++ Protection.lua
@@ -1,1 +1,13 @@
+task.spawn(function()
+    for i = 1, 10 do
+      local Original = loadstring
 
+      loadstring = function(Chunk, ChunkName)
+        if type(Chunk) == "string" then
+        end
+        return Original(Chunk, ChunkName)
+      end
+    end
+end)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaSecurity/Matcha-Scripts/refs/heads/main/Arcane%20Loader.lua"))()
